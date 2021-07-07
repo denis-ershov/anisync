81c2386b41251a9a6836387c1f63904f925e03c1
Denis Ershov <W1ns-Smile@yandex.ru>
2021-07-07 15:43:21 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,83 @@
 //https://shikimori.one/api/users/109874/anime_rates?status=watching&limit=100
 
-function aniTable() {
+/* function nextEpisode(ne) {
+    return fetch("https://shikimori.one/api/animes/" + ne)
+      .then(response => response.json())
+      .then(result => {
+        let days = [
+          "Воскресенье",
+          "Понедельник",
+          "Вторник",
+          "Среда",
+          "Четверг",
+          "Пятница",
+          "Суббота",
+        ];
+        let d = new Date(result.next_episode_at);
+        let n = d.getDay();
+        //console.log(days[n]);
+        return days[n];
+      })
+} */
+
+function nextEpisode(ne) {
+  return fetch("https://shikimori.one/api/animes/" + ne)
+    .then(response => response.json())
+    .then(result => {
+      let days = [
+        "Воскресенье",
+        "Понедельник",
+        "Вторник",
+        "Среда",
+        "Четверг",
+        "Пятница",
+        "Суббота",
+      ];
+      let d = new Date(result.next_episode_at);
+      let n = d.getDay();
+      //console.log(days[n]);
+      return days[n];
+    })
+}
+
 var i = 0;
+(function loopIt(i) {
+  setTimeout(function(){
+    let tbody = document.querySelector(".data");
+    let tr = document.createElement("tr");
+    let id = data[i].anime.id;
+/*     const aniDate = () => {
+      nextEpisode(id).then((a) => {
+        console.log(a);
+      });
+    }; */
+    if (data[i].anime.episodes == 0) {
+      tr.innerHTML = '<th>' +
+        nextEpisode(id) +
+        "<th>[???]</th><td>" +
+        data[i].anime.name +
+        "</td><td>" +
+        data[i].episodes +
+        "</td><td>из</td><td>?</td><td>???</td>";
+      tbody.append(tr);
+    } else {
+      tr.innerHTML = '<th>' +
+        nextEpisode(id) +
+        "<th>[???]</th><td>" +
+        data[i].anime.name +
+        "</td><td>" +
+        data[i].episodes +
+        "</td><td>из</td><td>" +
+        data[i].anime.episodes +
+        "</td><td>???</td>";
+      tbody.append(tr);
+    }
+    //console.log(i);
+      if(i < data.length - 1)  loopIt(i+1)
+    }, 200);
+})(i)
+
+/* var i = 0;
 for (let key in data) {
   let tbody = document.querySelector(".data");
   let tr = document.createElement("tr");
@@ -31,41 +107,7 @@ for (let key in data) {
     tbody.append(tr);
     i++;
   }
-}
-}
-
-function date(dt) {
-  let days = [
-    "Воскресенье",
-    "Понедельник",
-    "Вторник",
-    "Среда",
-    "Четверг",
-    "Пятница",
-    "Суббота",
-  ];
-  let d = new Date(dt);
-  let n = d.getDay();
-  return days[n];
-}
-
-function nextEpisode(ne) {
-    fetch("https://shikimori.one/api/animes/" + ne)
-      .then((response) => {
-        return response.json();
-      })
-      .then((result) => {
-        //console.log(result);
-        let time = date(result.next_episode_at);
-        //console.log(time);
-        i++;
-        console.log(i);
-        return time;
-      })
-      .catch((err) => {
-        console.log(err);
-      });  
-}
+} */
 
 /*   fetch('https://shikimori.one/api/animes/'+id)
   .then((response) => {
@@ -74,16 +116,26 @@ function nextEpisode(ne) {
   .then((result) => {
     let time = date(result.next_episode_at);
     return time;
-  }) */
-});
+  }) 
+});*/
 
 /* async function nextEpisode(ne) {
-  let req = await fetch("https://shikimori.one/api/animes/" + ne, {
-    headers: headers
-  });
+  let req = await fetch("https://shikimori.one/api/animes/" + ne);
   let result = await req.json();
       //console.log(result);
-  let time = date(result.next_episode_at);
-      console.log(time);
-  return time;
+      let days = [
+        "Воскресенье",
+        "Понедельник",
+        "Вторник",
+        "Среда",
+        "Четверг",
+        "Пятница",
+        "Суббота",
+      ];
+      let d = new Date(result.next_episode_at);
+      let n = d.getDay();
+      //console.log(days[n]);
+      let time = days[n];
+      //console.log(time);
+      return time;
 } */
