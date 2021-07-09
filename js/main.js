ff50430224eb50c4598314f4b1656a8cdc06a9ed
Denis Ershov <W1ns-Smile@yandex.ru>
2021-07-09 15:42:00 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,52 +1,104 @@
-//https://shikimori.one/api/users/109874/anime_rates?status=watching&limit=100
+const watchList =
+  "https://shikimori.one/api/users/109874/anime_rates?status=watching&limit=100";
 
-function nextEpisode(ne) {
-  return fetch("https://shikimori.one/api/animes/" + ne)
-    .then(response => response.json())
-    .then(result => {
-      let days = [
-        "Воскресенье",
-        "Понедельник",
-        "Вторник",
-        "Среда",
-        "Четверг",
-        "Пятница",
-        "Суббота",
-      ];
-      let d = new Date(result.next_episode_at);
-      let n = d.getDay();
-      return days[n];
-    })
+function animeList(url) {
+  return fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      let list = {};
+      let i = 0;
+      for (key in result) {
+        list[i] = {
+          id: result[i].anime.id,
+          episodes: result[i].episodes,
+        };
+        i++;
+      }
+      //console.log(list);
+      return list;
+    });
 }
+
+//animeList(watchList);
+
+function nextEpisode(time) {
+  let days = [
+    "Воскресенье",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+  ];
+  let d = new Date(time);
+  let n = d.getDay();
+  return days[n];
+}
+
+function animeData(id, episodes) {
+  return fetch("https://shikimori.one/api/animes/" + id)
+    .then((response) => response.json())
+    .then((result) => {
+      let data = {};
+      let i = 0;
+      for (key in result) {
+        data[i] = {
+          id: id,
+          name: result.name,
+          url: result.url,
+          next_episode: nextEpisode(result.next_episode_at),
+          dub: result.licensors,
+          sub: result.fandubbers,
+          watch_episodes: episodes,
+          all_episodes: result.episodes,
+        };
+        i++;
+      }
+      //console.log(data);
+      return data;
+    });
+}
+
+//animeData(48406);
+
+const info = animeList(watchList);
 
 var i = 0;
 (function loopIt(i) {
-  setTimeout( async function(){
+  setTimeout(async function () {
     let tbody = document.querySelector(".data");
     let tr = document.createElement("tr");
-    let id = data[i].anime.id;
-    if (data[i].anime.episodes == 0) {
-      tr.innerHTML = '<th>' +
-        await nextEpisode(id) +
-        "<th>[???]</th><td><a href='https://shikimori.one/"+ data[i].anime.url +"'>" +
-        data[i].anime.name +
-        "</td><td>" +
-        data[i].episodes +
-        "</td><td>из</td><td>?</td><td>???</td>";
-      tbody.append(tr);
+    let list = await info;
+    let data = await animeData(list[i].id, list[i].episodes);
+    let dub = "";
+    if (data[i].dub[0] == "Wakanim") {
+      dub = "WAKANIM";
     } else {
-      tr.innerHTML = '<th>' +
-        await nextEpisode(id) +        
-        "<th>[???]</th><td><a href='https://shikimori.one/"+ data[i].anime.url +"'>" +
-        data[i].anime.name +
-        "</td><td>" +
-        data[i].episodes +
-        "</td><td>из</td><td>" +
-        data[i].anime.episodes +
-        "</td><td>???</td>";
-      tbody.append(tr);
+      dub = "???";
     }
-    //console.log(i);
-      if(i < data.length - 1)  loopIt(i+1)
-    }, 200);
-})(i)
+    let allEpisodes = "";
+    if (data[i].all_episodes == 0) {
+      allEpisodes = "?";
+    } else {
+      allEpisodes = data[i].all_episodes;
+    }
+
+    tr.innerHTML =
+      "<th>" +
+      data[i].next_episode +
+      "<th>[" +
+      dub +
+      "]</th><td><a href='https://shikimori.one/" +
+      data[i].url +
+      "'>" +
+      data[i].name +
+      "</td><td>" +
+      data[i].watch_episodes +
+      "</td><td>из</td><td>" +
+      allEpisodes +
+      "</td><td> </td>";
+    tbody.append(tr);
+    if (i < Object.keys(list).length - 1) loopIt(i + 1);
+  }, 210);
+})(i);
