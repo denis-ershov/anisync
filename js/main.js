7a99869f892a5423cad9a2f0ea04404f488fa290
Denis Ershov <W1ns-Smile@yandex.ru>
2021-07-19 16:16:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const watchList =
   "https://shikimori.one/api/users/109874/anime_rates?status=watching&limit=100";
 
+  const seasons = ['Зима', 'Весна', 'Лето', 'Осень'];
+
+  function delay() {
+    return new Promise(resolve => setTimeout(resolve, 210));
+  }
+
 function animeList(url) {
   return fetch(url)
     .then((response) => response.json())
@@ -11,6 +17,7 @@ function animeList(url) {
         list[i] = {
           id: result[i].anime.id,
           episodes: result[i].episodes,
+          release_date: result[i].anime.aired_on
         };
         i++;
       }
@@ -36,7 +43,56 @@ function nextEpisode(time) {
   return days[n];
 }
 
+function getSeason(date) {
+  let i = 0;
+  if (date.getMonth() <= 1 || date.getMonth() == 11) {
+  i = 0;
+  //return i;
+  }
+  if (date.getMonth() >= 2 && date.getMonth() <= 4) {
+  i = 1;
+  //return i;
+  }
+  if (date.getMonth() >= 5 && date.getMonth() <= 7) {
+  i = 2;
+  //return i;
+  }
+  if (date.getMonth() >= 8 && date.getMonth() <= 10) {
+  i = 3;
+  //return i;
+  }
+  return i;
+  }
+
 function animeData(id, episodes) {
+  return fetch("https://shikimori.one/api/animes/" + id)
+    .then((response) => response.json())
+    .then((result) => {
+      let data = {};
+      let i = 0;
+        data = {
+          id: id,
+          name: result.name,
+          url: result.url,
+          next_episode: nextEpisode(result.next_episode_at),
+          dub: result.licensors,
+          sub: result.fandubbers,
+          watch_episodes: episodes,
+          all_episodes: result.episodes,
+          score: result.score
+        }
+        i++;
+      //console.log(data);
+      return data;
+    });
+}
+
+//animeData(48406);
+
+const info = animeList(watchList);
+
+/* async function animeData(id, episodes) {
+  await delay();
   return fetch("https://shikimori.one/api/animes/" + id)
     .then((response) => response.json())
     .then((result) => {
@@ -58,28 +114,31 @@ function animeData(id, episodes) {
     });
 }
 
-//animeData(48406);
-
-const info = animeList(watchList);
-
-/* async function animeBase() {
+async function processArray(obj) {
   let base = {};
-  let i = 0;
-  let list = await info;
-  let y = 0;
-(function loopIt(i) {
-  setTimeout(async function () {
-     base[i] = await animeData(list[i].id, list[i].episodes);
-     y++;
-     if (y == Object.keys(list).length) {
-      return(base);
-      }
-   if (i < Object.keys(list).length - 1) loopIt(i + 1);
-  }, 210);
-})(i);
+  let list = await obj;
+  //console.log(list);
+  for (const item in list) {
+    let now = new Date();
+    let onair = new Date(list[item].release_date);
+
+    if (now.getFullYear() - onair.getFullYear() > 1) {
+      base['Основное'] = Object.assign({[item]: await animeData(list[item].id, list[item].episodes)}, base['Основное']);
+    }
+    if (now.getFullYear() - onair.getFullYear() <= 1 && seasons[getSeason(onair)] == 'Зима') {
+      base['Зима'] = Object.assign({[item]: await animeData(list[item].id, list[item].episodes)}, base['Зима']);
+    }
+    if (now.getFullYear() - onair.getFullYear() == 0) {
+      base[seasons[getSeason(onair)]] = Object.assign({[item]: await animeData(list[item].id, list[item].episodes)}, base[seasons[getSeason(onair)]]);
+    }
+    //console.log(list[item]);
+  }
+  console.log(base);
+  console.log('Done!');
+  return base;
 }
 
-const json = animeBase();
+const json = processArray(info);
 
 let tbody = document.querySelector(".data");
     let tr = document.createElement("tr");
@@ -97,22 +156,23 @@ let tbody = document.querySelector(".data");
     } else {
       allEpisodes = data[i].all_episodes;
     }
-
     tr.innerHTML =
       "<th>" +
-      data[i].next_episode +
+      data.next_episode +
       "<th>[" +
       dub +
       "]</th><td><a href='https://shikimori.one/" +
-      data[i].url +
+      data.url +
       "'>" +
-      data[i].name +
+      data.name +
+      "</td><td>" + data.score +
       "</td><td>" +
-      data[i].watch_episodes +
+      data.watch_episodes +
       "</td><td>из</td><td>" +
       allEpisodes +
       "</td><td> </td>";
     tbody.append(tr); */
+
 
 var i = 0;
 (function loopIt(i) {
@@ -144,6 +204,7 @@ var i = 0;
       data.url +
       "'>" +
       data.name +
+      "</td><td>" + data.score +
       "</td><td>" +
       data.watch_episodes +
       "</td><td>из</td><td>" +
