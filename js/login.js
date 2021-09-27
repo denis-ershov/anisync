bc6271fd42f6025b17aa89969997c6152f7015a3
Denis Ershov <W1ns-Smile@yandex.ru>
2021-09-27 16:53:22 +0300

    Update

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-var url = 'https://graphql.anilist.co',
+/* var url = 'https://graphql.anilist.co',
     options = {
         method: 'POST',
         headers: {
@@ -15,4 +15,4 @@ fetch(url, options).then(handleResponse, handleError);
 
 function handleResponse (response) {
     console.log(response);
-}
+} */
