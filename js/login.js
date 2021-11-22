a173bea837fa385fec490ab709fedf4ffdfa3b0e
Denis Ershov <W1ns-Smile@yandex.ru>
2021-11-22 13:32:11 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,18 +1,22 @@
-/* var url = 'https://graphql.anilist.co',
-    options = {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Bearer ' + accessToken,
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: JSON.stringify({
-            query: query
-        })
-    };
+const code = location.search.substring(6);
+const url = 'https://shikimori.one/oauth/token';
+function accessToken (code) {
+    return fetch(url, {  
+        method: 'post',  
+        headers: {  
+          "User-Agent": "AniSync"  
+        },  
+        body: 'grant_type="authorization_code"&client_id="VR54LgcFVBy7f7skFdXd7y7pIC4XZKpkncJ2av38_Ic"&client_secret="EtGBP7rD5cLX35BajNvxbfry-2z43EXKsZXR-c0QxZg"&code='+code+'redirect_uri="https://denis-ershov.github.io/anisync/"'  
+      })
+      .then(json)  
+      .then(function (data) {  
+        console.log('Request succeeded with JSON response', data);  
+      })  
+      .catch(function (error) {  
+        console.log('Request failed', error);  
+      })
+}
 
-fetch(url, options).then(handleResponse, handleError);
-
-function handleResponse (response) {
-    console.log(response);
-} */
+if (code != null) {
+    accessToken(code);
+}
