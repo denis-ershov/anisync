a68b0d82de512f91431bb23f9cc13f593cfc99cb
Denis Ershov <W1ns-Smile@yandex.ru>
2021-11-22 14:17:29 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -9,13 +9,10 @@ function accessToken (code) {
         },  
         body: 'grant_type="authorization_code"&client_id="VR54LgcFVBy7f7skFdXd7y7pIC4XZKpkncJ2av38_Ic"&client_secret="EtGBP7rD5cLX35BajNvxbfry-2z43EXKsZXR-c0QxZg"&code='+code+'redirect_uri="https://denis-ershov.github.io/anisync/"'  
       })
-      .then(json)  
-      .then(function (data) {  
-        console.log('Request succeeded with JSON response', data);  
-      })  
-      .catch(function (error) {  
-        console.log('Request failed', error);  
-      })
+      .then((response) => response.json())
+    .then((result) => {
+        console.log(result);
+    });
 }
 
 if (code !== '') {
