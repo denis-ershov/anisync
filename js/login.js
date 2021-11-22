7955884ae19cfae61a0f5235888daa8d40387cdd
Denis Ershov <W1ns-Smile@yandex.ru>
2021-11-22 14:48:47 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 const code = location.search.substring(6);
 console.log(code);
-const url = 'https://shikimori.one/oauth/token?grant_type=authorization_code&client_id=VR54LgcFVBy7f7skFdXd7y7pIC4XZKpkncJ2av38_Ic&client_secret=EtGBP7rD5cLX35BajNvxbfry-2z43EXKsZXR-c0QxZg&code='+code+'redirect_url=https://denis-ershov.github.io/anisync/';
+const url = 'https://shikimori.one/oauth/token?grant_type=authorization_code&client_id=VR54LgcFVBy7f7skFdXd7y7pIC4XZKpkncJ2av38_Ic&client_secret=EtGBP7rD5cLX35BajNvxbfry-2z43EXKsZXR-c0QxZg&code='+code+'&redirect_uri=https://denis-ershov.github.io/anisync/';
 const options = {  
     method: 'post',  
     headers: {  
